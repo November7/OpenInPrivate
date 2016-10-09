3c07aca9ca07fbde4e0e72ecd56eabd253bf30a1
November7 <m.kowalski@zsme.tarnow.pl>
2016-10-09 12:51:34 +0200

    Update 20161009

--- bkg.js
+++ bkg.js
@@ -1,30 +1,27 @@
 
-(function()
-{
-	var background = browser.extension.getBackgroundPage();
-	background.focused = false;
-	browser.storage.local.get("openTop",function (element) {
-		background.focused = element.openTop?true:false;
-	});
+var focused = false;
+browser.storage.local.get("openTop",function (element) {
+	focused = element.openTop?true:false;
+});
 
 
-	var title = browser.i18n.getMessage("menuName");
-	// ???
-	if(!title) title = "open as inPrivate";
+var title = browser.i18n.getMessage("menuName");
+
+if(!title) title = "open as inPrivate";
+
+
+
+browser.contextMenus.create({"title": title, "id": "parent", "contexts": ["link"]});
+
+browser.contextMenus.onClicked.addListener(function(info, tab) {			
+	var params = {
+		url: info.linkUrl,
+		focused: focused,			
+		inPrivate: true
+	};
+	browser.windows.create(params);
 	
-	
+});	
 
-	browser.contextMenus.create({"title": title, "id": "parent", "contexts": ["link"]});
-
-	browser.contextMenus.onClicked.addListener(function(info, tab) {			
-		var params = {
-			url: info.linkUrl,
-			focused: background.focused,			
-			inPrivate: true
-		};
-		browser.windows.create(params);
-		
-	});	
-})();
 
 
--- popup.js
+++ popup.js
@@ -1,24 +1,22 @@
-(function(){
-	var onTopCheckbox = document.getElementById('openTop');	
-	
-	var background = browser.extension.getBackgroundPage(); 
-	onTopCheckbox.checked = background.focused;
 
-	
-	var objects = document.getElementsByTagName('*');
-	for(var i = 0; i < objects.length; i++)
+var onTopCheckbox = document.getElementById('openTop');	
+
+var background = browser.extension.getBackgroundPage(); 
+onTopCheckbox.checked = background.focused;
+
+
+var objects = document.getElementsByTagName('*');
+for(var i = 0; i < objects.length; i++)
+{
+	if (objects[i].dataset && objects[i].dataset.message) 
 	{
-		if (objects[i].dataset && objects[i].dataset.message) 
-		{
-			objects[i].innerHTML = browser.i18n.getMessage(objects[i].dataset.message);
-		}
+		objects[i].innerHTML = browser.i18n.getMessage(objects[i].dataset.message);
 	}
-	
-	if(onTopCheckbox) {
-		onTopCheckbox.addEventListener("click", function() {
-			background.focused = this.checked;
-			browser.storage.local.set({openTop: this.checked});
-		});	
-	}
-	
-})();
+}
+
+if(onTopCheckbox) {
+	onTopCheckbox.addEventListener("click", function() {
+		background.focused = this.checked;
+		browser.storage.local.set({openTop: this.checked});
+	});	
+}
